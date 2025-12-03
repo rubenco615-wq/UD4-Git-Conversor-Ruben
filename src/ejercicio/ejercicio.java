d2d124063d0163d7fc10e6dd24b068e406ee3223
ruben castillo <rubenco615@alumnos.ilerna.com>
2025-12-03 14:05:05 +0100

    cambios del proyecto

---
+++ src/ejercicio/ejercicio.java
@@ -0,0 +1,25 @@
+package ejercicio;
+import java.util.*;
+public class ejercicio {
+
+
+	public class ConversorTemperatura {
+
+	    public static double celsiusAFahrenheit(double c) {
+	        return c * 9.0 / 5.0 + 32.0;
+	    }
+
+	    public static double fahrenheitACelsius(double f) {
+	        return (f - 32.0) * 5.0 / 9.0;
+	    }
+
+	    public static void main(String[] args) {
+	        System.out.println("0°C = " + celsiusAFahrenheit(0) + "°F");
+	        System.out.println("32°F = " + fahrenheitACelsius(32) + "°C");
+	   }
+	}
+}
+	
+
+	
+	
